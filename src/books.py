60b0a6c50f8d36e280de9ac4954c74dd321a25c1
Rafael Ortiz <rafael@helloinsight.org>
2024-03-22 17:14:11 -0500

    path parameter

--- src/books.py
+++ src/books.py
@@ -4,5 +4,12 @@ from data.books import BOOKS
 app = FastAPI()
 
 @app.get('/books')
-async def getBooks():
+async def get_books():
     return BOOKS
+
+# path parameters are request parameters attached to the url
+@app.get('/books/{book_title}')
+async def get_params(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
